d83e553f2f37ad7f4c0527ca7e1ff5efe1aef036
daniellutziger <daniel.lutziger@uzh.ch>
2025-02-12 15:59:55 +0100

    cleaned up.

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import React, {useState} from "react";
 import {CssBaseline} from "@mui/material";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import {HashRouter as Router, Routes, Route, Navigate} from "react-router-dom";
 import ProductDetail from "./components/ProductDetail";
 import Homepage from "./components/Homepage.jsx";
