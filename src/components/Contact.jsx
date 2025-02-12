98f522e49df3b99a9c7143a43006b08211c8ae23
daniellutziger <daniel.lutziger@uzh.ch>
2025-02-12 16:11:04 +0100

    added anliegen mitteilen.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,56 @@
 import "react";
-import { Box, Typography } from "@mui/material";
+import {Box, Button, IconButton, Modal, TextField, Typography} from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import {MuiTelInput} from "mui-tel-input";
+import Textarea from "@mui/joy/Textarea";
+import React, {useState} from "react";
+import {api} from "../utils/api.jsx";
 
 export default function ContactPage() {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    const [formDetails, setFormDetails] = useState({
+        firstname: "",
+        email: "",
+        phone: "",
+        bemerkung: "",
+        phoneError: false,
+        emailError: false
+    });
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormDetails({ ...formDetails, [name]: value });
+    };
+
+    const isFormValid = () => {
+        return (
+            formDetails.firstname.trim() &&
+            formDetails.email.trim() &&
+            formDetails.phone.trim() &&
+            !formDetails.phoneError &&
+            !formDetails.emailError
+        );
+    };
+
+    const handleFormSubmit = () => {
+        if (isFormValid()) {
+            api.post('/api/anliegen_melden', formDetails)
+                .then(response => {
+                    console.log('Email sent:', response.data);
+                    setFormDetails({ firstname: "", lastname: "", email: "", phone: "" });
+                    handleClose();
+                })
+                .catch(error => {
+                    console.error('Failed to send email:', error);
+                    alert(error.response.data.detail);
+                });
+        } else {
+            alert("Alle Felder ausfüllen bitte.");
+        }
+    };
+
     return (
         <div>
             <div className={"siteHeader"}>
@@ -90,6 +139,159 @@ export default function ContactPage() {
                     <Typography variant="body2">Montag - Freitag: 9:00 - 18:30</Typography>
                     <Typography variant="body2">Samstag: 09:00 - 16:00</Typography>
                     <Typography variant="body2">Sonntag: Geschlossen</Typography>
+
+                    {/* Anliegen mitteilen */}
+                    <>
+                        <Button variant="contained" color="secondary" onClick={handleOpen} sx={{ width: "100%", mt:3 }}
+                                className={"buttonColor"}
+                        >
+                            Anliegen mitteilen
+                        </Button>
+                        <Modal
+                            open={open}
+                            onClose={handleClose}
+                            aria-labelledby="modal-modal-title"
+                            aria-describedby="modal-modal-description"
+                        >
+                            <Box
+                                sx={{
+                                    position: "absolute",
+                                    top: "50%",
+                                    left: "50%",
+                                    transform: "translate(-50%, -50%)",
+                                    width: "90%", // Adjust width as needed
+                                    maxWidth: "600px",
+                                    maxHeight: "90%", // Ensure it doesn't overflow vertically
+                                    bgcolor: "white",
+                                    boxShadow: 24,
+                                    borderRadius: "8px",
+                                    overflow: "auto", // Make content scrollable
+                                }}
+                            >
+                                <Box
+                                    sx={{
+                                        display: "flex",
+                                        justifyContent: "space-between",
+                                        alignItems: "center",
+                                        p: 2,
+                                        borderBottom: "1px solid #ddd",
+                                    }}
+                                >
+                                    <Typography variant="h4">
+                                        Anliegen mitteilen
+                                    </Typography>
+                                    <IconButton onClick={handleClose} size="small">
+                                        <CloseIcon />
+                                    </IconButton>
+                                </Box>
+                                <Box sx={{p:3}}>
+
+                                    <TextField
+                                        label="Vorname"
+                                        name="firstname"
+                                        className={"textfieldActive"}
+                                        value={formDetails.firstname}
+                                        onChange={handleInputChange}
+                                        required
+                                        fullWidth
+                                        sx={{ mb: 2 }}
+                                    />
+                                    <TextField
+                                        label="E-Mail"
+                                        name="email"
+                                        className={"textfieldActive"}
+                                        value={formDetails.email}
+                                        onChange={(e) => {
+                                            const { name, value } = e.target;
+                                            // Validate the email format
+                                            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+                                            const isValidEmail = emailRegex.test(value);
+
+                                            // Update the form details, including the error flag
+                                            setFormDetails({
+                                                ...formDetails,
+                                                [name]: value,
+                                                emailError: !isValidEmail && value !== "", // Set error only if not valid and not empty
+                                            });
+                                        }}
+                                        required
+                                        fullWidth
+                                        sx={{ mb: 2 }}
+                                        error={formDetails.emailError} // Use emailError for validation
+                                        helperText={
+                                            formDetails.emailError
+                                                ? "Bitte geben Sie eine gültige E-Mail-Adresse ein."
+                                                : ""
+                                        }
+                                    />
+                                    <MuiTelInput
+                                        id="phone"
+                                        label="Telefonnummer"
+                                        placeholder="Telefonnummer *"
+                                        name="phone"
+                                        className={"textfieldActive"}
+                                        value={formDetails.phone}
+                                        onChange={(value) => {
+                                            // Define regex for Swiss, German, and Austrian phone numbers
+                                            const swissPhoneRegex = /^(\+41) ([1-9]{1}[0-9]{1}) [0-9]{3} [0-9]{2} [0-9]{2}$/; // e.g., +41 79 123 45 67
+                                            const germanPhoneRegex = /^(\+49) ([1-9]{4}) [0-9]{5,}$/; // e.g., +49 1234 12345
+                                            const austrianPhoneRegex = /^(\+43) ([1-9]{4}) [0-9]{5,}$/; // e.g., +43 1234 12345
+
+                                            // Check if the input matches any of the formats
+                                            const isValid =
+                                                swissPhoneRegex.test(value) ||
+                                                germanPhoneRegex.test(value) ||
+                                                austrianPhoneRegex.test(value);
+
+                                            setFormDetails({
+                                                ...formDetails,
+                                                phone: value,
+                                                phoneError: !isValid, // Add an error flag for validation
+                                            });
+                                        }}
+                                        required
+                                        fullWidth
+                                        sx={{ mb: 1 }}
+                                        defaultCountry="CH"
+                                        error={formDetails.phoneError}
+                                        helperText={
+                                            formDetails.phoneError
+                                                ? "Bitte geben Sie eine gültige Telefonnummer aus der Schweiz, Deutschland oder Österreich ein."
+                                                : ""
+                                        }
+                                    />
+
+                                    <Textarea
+                                        minRows={4}
+                                        label="Bemerkung"
+                                        placeholder={"Wie können wir die weiterhelfen?"}
+                                        name="bemerkung"
+                                        variant="outlined"
+                                        className={"textfieldActive"}
+                                        value={formDetails.bemerkung}
+                                        onChange={(e) => {
+                                            const { name, value } = e.target;
+                                            setFormDetails({
+                                                ...formDetails,
+                                                [name]: value
+                                            });
+                                        }}
+                                        fullWidth
+                                        sx={{ mb: 2, mt:1 }}
+                                    />
+
+                                    <Button
+                                        className={"buttonColor"}
+                                        onClick={handleFormSubmit}
+                                        disabled={!isFormValid()}
+                                        fullWidth
+                                    >
+                                        Anliegen mitteilen
+                                    </Button>
+                                </Box>
+                            </Box>
+                        </Modal>
+                    </>
                 </Box>
             </Box>
         </div>
